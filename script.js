739f92a977ad1583fb7cff063dd9a42024d48887
shirone <kurenaishirone@gmail.com>
2026-03-27 11:08:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function getEl(id) {
   return document.getElementById(id);
 }
-const articleNumber = getEl("articleNumber");
-const result = getEl("result");
+let articleNumber, result;
+window.addEventListener("load", () => {
+  articleNumber = getEl("articleNumber");
+  result = getEl("result");
+})
 async function search() {
+  
   const response = await fetch(`https://api.kurenaishirone.xyz/locate?num=${articleNumber.value}`);
   const data = await response.json();
   result.textContent = data;
